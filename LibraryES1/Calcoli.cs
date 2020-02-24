cc51b090fe86299fef67da390b2d298baf4c72e8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-24 16:09:19 +0100

    Aggiungere i file di progetto.

---
+++ LibraryES1/Calcoli.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryES1
+{
+    public class Calcoli
+    {
+        public static bool QuadratoP(int N2)
+        {
+            for (int i = 1; i<= N2; i++)
+            {
+                int q = i * i;
+                if (q == N2)
+                    return true;
+                if(q > N2)
+                    return false; 
+            }
+            return false;
+        }//fare test a casa
+    }
+}
---
+++ TestLibreria/Program.cs
@@ -0,0 +1,29 @@
+﻿using LibraryES1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestLibreria;
+
+namespace TestLibreria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserisci un numero");
+            int n = int.Parse(Console.ReadLine());
+            bool risposta = Calcoli.QuadratoP(n);
+            if (risposta)
+            {
+                Console.WriteLine("Il numero è un quadrato perfetto");
+            }
+            else
+            {
+                Console.WriteLine("Il numero non è un numero perfetto");
+            }
+            Console.ReadLine();
+        }
+    }
+}
